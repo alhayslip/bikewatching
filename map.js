fb62a290aaa765d9b20c845670376f9a26855691
AHayslip23 <alhayslip@ucsd.edu>
2025-11-09 14:18:48 -0800

    Step 2.1 fixes for map.js and index.html
    Step 2.1 fixes for map.js and index.html

--- map.js
+++ map.js
@@ -1,11 +1,21 @@
-import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
-console.log('Mapbox GL JS Loaded:', mapboxgl);
+import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@3.0.1/+esm';
 
-map.on('load', async () => {
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWxoYXlzbGlwIiwiYSI6ImNtaHM3MHVjODFncnoya3E0bmJtYWlnbjUifQ.fG7DieNrtbmKOuphePmtlw';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/alhayslip/cmhs8o3ly007n01su7kywabwn',
+  center: [-71.0589, 42.3601],
+  zoom: 12,
+  minZoom: 5,
+  maxZoom: 18
+});
+
+map.on('load', () => {
   if (!map.getSource('boston_route')) {
     map.addSource('boston_route', {
       type: 'geojson',
-      data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
+      data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson'
     });
   }
 
@@ -18,10 +28,9 @@ map.on('load', async () => {
         'line-color': 'orange',
         'line-width': 5,
         'line-opacity': 0.45,
-      },
+        'line-join': 'round',
+        'line-cap': 'round'
+      }
     });
   }
-
-  map.setCenter([-71.0589, 42.3601]);
-  map.setZoom(12);
 });
